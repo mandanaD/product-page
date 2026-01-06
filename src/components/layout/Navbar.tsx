0663d2563f62ffb2a0d8ba27a4db7e4db4b9f8a2
Mandana Dashti <dmandana59@gmail.com>
2026-01-06 22:54:05 +0330

    feat: create Navbar

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-300">
+            <button className="btn btn-ghost text-xl">NextJs</button>
+        </div>
+    )
+}
+export default Navbar;
