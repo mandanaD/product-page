9e57971fe7f6e1674efec27957c0e82b80208b52
Mandana Dashti <dmandana59@gmail.com>
2026-01-07 00:15:32 +0330

    feat: add disable & value

--- src/components/ui/rate/Rate.types.ts
+++ src/components/ui/rate/Rate.types.ts
@@ -3,5 +3,7 @@ import type {ComponentProps} from "react";
 export type RateProps = ComponentProps<"div"> & {
     shape?: "squircle" | "heart" | "hexagon" | "hexagon2" | "decagon" | "pentagon" | "star" | "starBold" | "circle";
     size?: "xs" | "sm" | "md" | "lg" | "xl",
-    length?:number
+    length?:number,
+    disabled?:boolean,
+    value?:number,
 };
