ece7dab48dbbf5d6678ca70228b81074bdf65d94
Mandana Dashti <dmandana59@gmail.com>
2026-01-07 01:51:10 +0330

    feat: add addReviewShema

--- src/shema/product.shema.ts
+++ src/shema/product.shema.ts
@@ -4,5 +4,13 @@ export const addCardShema=z.object({
     size:z.string().min(1,"Size is required"),
     color:z.string().min(1,"Color is required"),
 })
-
 export type AddCardType=z.infer<typeof addCardShema>;
+
+
+// review
+export const addReviewShema=z.object({
+    rate:z.number().min(1,"rate is required"),
+    full_name:z.string().min(1,"Full name is required"),
+    description:z.string().min(1,"Description is required"),
+})
+export type ReviewFormType=z.infer<typeof addReviewShema>;
