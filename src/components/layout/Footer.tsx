8728b3506315fcd25b1a15806ec98f9d0f46ca47
Mandana Dashti <dmandana59@gmail.com>
2026-01-06 22:53:57 +0330

    feat: create Footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+            <aside>
+                <p>Copyright © 2026 - All right reserved by NextJs</p>
+            </aside>
+        </footer>
+    )
+}
+export default Footer;
