a09d8e3ddc8cf856a3e093567b6f059ab3c82ce8
Mandana Dashti <dmandana59@gmail.com>
2026-01-07 01:51:36 +0330

    feat: add ProductColor

--- src/components/pages/ProductReviews.tsx
+++ src/components/pages/ProductReviews.tsx
@@ -1,16 +1,17 @@
 "use client"
 import React from "react";
-import { PRODUCT_DATA } from "@/mock/product-data";
 import Rate from "@/components/ui/rate/Rate";
-import { useProduct } from "@/context/ProductContext";
+import {useProduct} from "@/context/ProductContext";
+import {ProductColor} from "@/type/product.type";
+import {ReviewForm} from "@/components/pages/ReviewForm";
 
-const ProductReviews = () => {
-    const { selectedColorId } = useProduct();
+const ProductReviews = ({colors}: { colors: ProductColor[] }) => {
+    const {selectedColorId} = useProduct();
 
     const reviews = selectedColorId
-        ? PRODUCT_DATA.colors.find(color => color.id === selectedColorId)
+        ? colors.find(color => color.id === selectedColorId)
         ?.reviews || []
-        : PRODUCT_DATA.colors.flatMap(color =>
+        : colors.flatMap(color =>
             color.reviews.map(review => ({
                 ...review,
                 colorName: color.name,
@@ -18,10 +19,12 @@ const ProductReviews = () => {
         );
 
     return (
-        <section className="y-12">
-            <h2 className="text-lg sm:text-xl lg:text-2xl font-bold text-gray-900 mb-6">
+        <section className="y-12 space-y-8">
+            <h2 className="text-lg sm:text-xl lg:text-2xl font-bold text-gray-900">
                 Customer Reviews
             </h2>
+
+            <ReviewForm/>
 
             {reviews.length === 0 ? (
                 <p className="text-gray-500">No reviews yet.</p>
@@ -30,23 +33,22 @@ const ProductReviews = () => {
                     {reviews.map((review, index) => (
                         <div
                             key={index}
-                            className="border border-base-300 rounded-lg p-5 space-y-2"
+                            className="border border-base-300 rounded-lg p-3 sm:p-5 space-y-2"
                         >
                             <div className="flex items-center justify-between">
                                 <div>
-                                    <p className="font-semibold text-gray-900">
-                                        {review.full_name}
+                                    <p className="sm:text-md text-sm font-semibold text-gray-900">
+                                        {review.full_name || "-"}
                                     </p>
                                     <p className="text-xs text-gray-500">
-                                        {review.date}
+                                        {review.date || "-"}
                                     </p>
                                 </div>
-
-                                <Rate disabled={true} value={review.rating} size="sm" />
+                                <Rate disabled={true} value={review.rating} size="sm"/>
                             </div>
 
-                            <p className="text-sm text-gray-700">
-                                {review.comment || "No comment provided."}
+                            <p className="text-xs sm:text-sm text-gray-700">
+                                {review.comment || "-"}
                             </p>
                         </div>
                     ))}
